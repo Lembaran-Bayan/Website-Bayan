e297cccb058bc948a201cbaf067b95404fae202d
Ahmad Zaki Akmal <87590846+ahmadzakiakmal@users.noreply.github.com>
2024-07-03 21:05:55 +0700

    feat: improve responsiveness

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,12 +6,13 @@ import { useRouter } from "next/router";
 export default function Navbar() {
   const router = useRouter();
   return(
-    <nav className="fixed top-0 py-5 px-[100px] flex w-full justify-between text-white items-center">
+    <nav className="fixed top-0 py-5 px-[20px] md:px-[50px] lg:px-[100px] flex w-full justify-between text-white items-center">
+      <Link href="/">
       <div className="flex font-medium text-white items-center gap-2">
         <Image src={LogoWhite} alt="" className="size-[48px]" />
         <h1 className="text-shadow-2 text-[25px]">Lembaran <span className="font-bold">Bayan</span></h1>
-      </div>
-      <div className="text-shadow-2 flex gap-7">
+      </div></Link>
+      <div className="text-shadow-2 lg:flex gap-7 hidden">
         <Link href="/" className="after:w-full after:h-[3px] after:bg-white after:absolute relative after:top-[100%] after:left-0 //after:shadow-[0_0_5px_#000]">Beranda</Link>
         <Link href="/coming-soon" className="after:h-[3px] after:bg-white after:absolute relative after:top-[100%] after:left-0 after:w-[0px] hover:after:w-full after:transition-width after:duration-200">Profil</Link>
         <Link href="/coming-soon" className="after:h-[3px] after:bg-white after:absolute relative after:top-[100%] after:left-0 after:w-[0px] hover:after:w-full after:transition-width after:duration-200">Wisata dan Budaya</Link>
