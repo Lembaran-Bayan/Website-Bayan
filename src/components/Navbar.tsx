e0af668c183293d678e73f83250ca80a0ce12bf2
Ahmad Zaki Akmal <87590846+ahmadzakiakmal@users.noreply.github.com>
2024-07-03 20:55:46 +0700

    feat: add navbar menu styles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,21 @@
 import LogoWhite from "@/../public/LogoWhite.png";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Navbar() {
+  const router = useRouter();
   return(
     <nav className="fixed top-0 py-5 px-[100px] flex w-full justify-between text-white items-center">
       <div className="flex font-medium text-white items-center gap-2">
         <Image src={LogoWhite} alt="" className="size-[48px]" />
         <h1 className="text-shadow-2 text-[25px]">Lembaran <span className="font-bold">Bayan</span></h1>
       </div>
-      <div className="text-shadow-2 flex gap-6">
-        <Link href="/coming-soon">Beranda</Link>
-        <Link href="/coming-soon">Profil</Link>
-        <Link href="/coming-soon">Wisata dan Budaya</Link>
-        <Link href="/coming-soon">Kegiatan</Link>
+      <div className="text-shadow-2 flex gap-7">
+        <Link href="/" className="after:w-full after:h-[3px] after:bg-white after:absolute relative after:top-[100%] after:left-0 //after:shadow-[0_0_5px_#000]">Beranda</Link>
+        <Link href="/coming-soon" className="after:h-[3px] after:bg-white after:absolute relative after:top-[100%] after:left-0 after:w-[0px] hover:after:w-full after:transition-width after:duration-200">Profil</Link>
+        <Link href="/coming-soon" className="after:h-[3px] after:bg-white after:absolute relative after:top-[100%] after:left-0 after:w-[0px] hover:after:w-full after:transition-width after:duration-200">Wisata dan Budaya</Link>
+        <Link href="/coming-soon" className="after:h-[3px] after:bg-white after:absolute relative after:top-[100%] after:left-0 after:w-[0px] hover:after:w-full after:transition-width after:duration-200">Kegiatan</Link>
       </div>
     </nav>
   )
