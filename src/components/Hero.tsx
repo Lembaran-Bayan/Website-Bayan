368c03967ca3010c57ce73b5de85224bdb82aed4
Ahmad Zaki Akmal <87590846+ahmadzakiakmal@users.noreply.github.com>
2024-07-03 20:04:07 +0700

    feat: create Hero component

---
+++ src/components/Hero.tsx
@@ -0,0 +1,11 @@
+export default function Hero() {
+  return (
+    <main className="min-h-screen bg-[url('/Rinjani.png')] bg-cover bg-center flex justify-center items-center">
+      <h1 className="text-center text-[28px] text-shadow-1 text-white font-medium">
+        <span>Memulai Lembaran</span>
+        <br />
+        <span className="text-[40px]">Dengan Keindahan <span className="font-bold">Bayan</span></span>
+      </h1>
+    </main>
+  );
+}
