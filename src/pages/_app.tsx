c0a18315c493c75f4ab11e1f8c77b0c9fa107e97
Ahmad Zaki Akmal <87590846+ahmadzakiakmal@users.noreply.github.com>
2024-07-05 06:35:16 +0700

    feat: add title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
     <Head>
       <link rel="icon" href="/Logo.png" />
+      <title>Lembaran Bayan</title>
     </Head>
     <Component {...pageProps} />
     </>
