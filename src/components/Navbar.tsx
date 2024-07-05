f81a6eeb15c6dd831e29b4d9193fe0f100cbd2d6
Ahmad Zaki Akmal <87590846+ahmadzakiakmal@users.noreply.github.com>
2024-07-05 13:52:36 +0700

    fix: missing hidden class

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -64,7 +64,7 @@ export default function Navbar() {
         </Link>
       </div>
 
-      <div className="relative flex items-center">
+      <div className="relative flex items-center lg:hidden">
         <button
           className="w-[25px] lg:hidden"
           onClick={() => {
