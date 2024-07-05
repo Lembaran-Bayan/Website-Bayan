31f9514b38261bc897e9fc4bbec08164ec263f04
Ahmad Zaki Akmal <87590846+ahmadzakiakmal@users.noreply.github.com>
2024-07-05 07:54:06 +0700

    feat: add more AOS

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,10 @@
 export default function Hero() {
   return (
     <main className="min-h-screen bg-[url('/Rinjani.png')] bg-cover bg-center flex justify-center items-center">
-      <h1 data-aos="fade-up" className="text-center text-[24px] md:text-[28px] text-shadow-1 text-white font-medium p-5 sm:p-0">
-        <span>Memulai Lembaran</span>
-        <br />
-        <span className="text-[35px] md:text-[40px]">Dengan Keindahan <span className="font-bold">Bayan</span></span>
-      </h1>
+      <div className="text-center text-[24px] md:text-[28px] text-shadow-1 text-white font-medium p-5 sm:p-0">
+        <h2 data-aos="fade-up">Memulai Lembaran</h2>
+        <h2 data-aos="fade-up" data-aos-delay="400" className="text-[35px] md:text-[40px]">Dengan Keindahan <span className="font-bold">Bayan</span></h2>
+      </div>
     </main>
   );
 }
