246aa832bb7c551cc6d1adcdbd5b5a972821068e
Ahmad Zaki Akmal <87590846+ahmadzakiakmal@users.noreply.github.com>
2024-07-05 08:04:33 +0700

    fix: minor optimization

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,22 +11,56 @@ export default function Navbar() {
   const [scrollPerVH, setScrollPerVH] = useState<number>(0);
 
   useEffect(() => {
-    setScrollPerVH(scrollPosition / window.innerHeight)
-  }, [scrollPosition])
+    setScrollPerVH(scrollPosition / window.innerHeight);
+  }, [scrollPosition]);
 
-  return(
-    <nav data-aos="fade-down" data-aos-delay="0" className={"fixed top-0 py-5 px-[20px] md:px-[50px] lg:px-[100px] flex w-full justify-between text-white items-center z-[99]"} style={{background: `rgba(24,64,15,${scrollPerVH > 0.9 ? 1 : scrollPerVH})`}}>
+  return (
+    <nav
+      data-aos="fade-down"
+      data-aos-delay="0"
+      className={
+        "fixed top-0 py-5 px-[20px] md:px-[50px] lg:px-[100px] flex w-full justify-between text-white items-center z-[99]"
+      }
+      style={{ background: `rgba(24,64,15,${scrollPerVH > 0.9 ? 1 : Number(scrollPerVH.toFixed(2))})` }}
+    >
       <Link href="/">
-      <div className="flex font-medium text-white items-center gap-2">
-        <Image src={LogoWhite} alt="" className="size-[48px]" />
-        <h1 className="text-shadow-2 text-[25px]">Lembaran <span className="font-bold">Bayan</span></h1>
-      </div></Link>
+        <div className="flex font-medium text-white items-center gap-2">
+          <Image
+            src={LogoWhite}
+            alt=""
+            className="size-[48px]"
+          />
+          <h1 className="text-shadow-2 text-[25px]">
+            Lembaran <span className="font-bold">Bayan</span>
+          </h1>
+        </div>
+      </Link>
       <div className="text-shadow-2 lg:flex gap-7 hidden">
-        <Link href="/" className="after:w-full after:h-[3px] after:bg-white after:absolute relative after:top-[100%] after:left-0 //after:shadow-[0_0_5px_#000]">Beranda</Link>
-        <Link href="/coming-soon" className="after:h-[3px] after:bg-white after:absolute relative after:top-[100%] after:left-0 after:w-[0px] hover:after:w-full after:transition-width after:duration-200">Profil</Link>
-        <Link href="/coming-soon" className="after:h-[3px] after:bg-white after:absolute relative after:top-[100%] after:left-0 after:w-[0px] hover:after:w-full after:transition-width after:duration-200">Wisata dan Budaya</Link>
-        <Link href="/coming-soon" className="after:h-[3px] after:bg-white after:absolute relative after:top-[100%] after:left-0 after:w-[0px] hover:after:w-full after:transition-width after:duration-200">Kegiatan</Link>
+        <Link
+          href="/"
+          className="after:w-full after:h-[3px] after:bg-white after:absolute relative after:top-[100%] after:left-0 //after:shadow-[0_0_5px_#000]"
+        >
+          Beranda
+        </Link>
+        <Link
+          href="/coming-soon"
+          className="after:h-[3px] after:bg-white after:absolute relative after:top-[100%] after:left-0 after:w-[0px] hover:after:w-full after:transition-width after:duration-200"
+        >
+          Profil
+        </Link>
+        <Link
+          href="/coming-soon"
+          className="after:h-[3px] after:bg-white after:absolute relative after:top-[100%] after:left-0 after:w-[0px] hover:after:w-full after:transition-width after:duration-200"
+        >
+          Wisata dan Budaya
+        </Link>
+        <Link
+          href="/coming-soon"
+          className="after:h-[3px] after:bg-white after:absolute relative after:top-[100%] after:left-0 after:w-[0px] hover:after:w-full after:transition-width after:duration-200"
+        >
+          Kegiatan
+        </Link>
       </div>
     </nav>
-  )
+  );
 }
