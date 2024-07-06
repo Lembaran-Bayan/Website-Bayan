fc89680c09f0eb4d4116c336a16b9351cb899c71
Ahmad Zaki Akmal <87590846+ahmadzakiakmal@users.noreply.github.com>
2024-07-06 17:52:49 +0700

    fix: optimize image to webp

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,6 @@
 export default function Hero() {
   return (
-    <main className="min-h-screen bg-[url('/Rinjani.jpg')] bg-cover bg-center flex justify-center items-center">
+    <main className="min-h-screen bg-[url('/Rinjani.webp')] bg-cover bg-center flex justify-center items-center">
       <div className="text-center text-[24px] md:text-[28px] text-shadow-1 text-white font-medium p-5 sm:p-0">
         <h2 data-aos="fade-up">Memulai Lembaran</h2>
         <h2 data-aos="fade-up" data-aos-delay="400" className="text-[30px] md:text-[40px]">Dengan Keindahan <span className="font-bold">Bayan</span></h2>
