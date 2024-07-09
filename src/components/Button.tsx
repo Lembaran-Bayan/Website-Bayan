13c34950b142441bd6c0a785e5a911059441d0b6
Ahmad Zaki Akmal <87590846+ahmadzakiakmal@users.noreply.github.com>
2024-07-09 20:59:03 +0700

    feat: create Button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,34 @@
+import { ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+  className?: string;
+  ariaLabel: string;
+  onClick?: () => any;
+  disabled?: boolean;
+}
+
+export default function Button({
+  children,
+  className,
+  ariaLabel = "Button",
+  onClick = () => console.log("Clicked"),
+  disabled = false,
+}: Props) {
+  return (
+    <button
+      aria-label={ariaLabel}
+      className={
+        "px-8 py-3 bg-green-1 text-white font-poppins font-medium rounded-full text-[17px] hover:bg-[#112D0B] transition hover:shadow-[0_0_5px_rgba(24,64,15,.6)] active:shadow-[0_0_8px_rgba(24,64,15,.9)] disabled:opacity-60 cursor-pointer disabled:cursor-not-allowed " +
+        className
+      }
+      onClick={() => {
+        if (disabled) return;
+        onClick();
+      }}
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  );
+}
