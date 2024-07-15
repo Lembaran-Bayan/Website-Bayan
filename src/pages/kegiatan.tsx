fa8386e27a559020212816cc5a1afa5c4d94394f
Ahmad Zaki Akmal <87590846+ahmadzakiakmal@users.noreply.github.com>
2024-07-15 18:24:39 +0700

    feat: add components

--- src/pages/kegiatan.tsx
+++ src/pages/kegiatan.tsx
@@ -3,6 +3,7 @@ import Kegiatan from "@/../public/Kegiatan.webp"
 import TopArticle from "@/components/TopArticle";
 import Footer from "@/components/Footer";
 import ArticleSlider from "@/components/ArticleSlider";
+import PostArticle from "@/components/PostArticle";
 
 export default function ProfilePage() {
   return (
@@ -33,7 +34,7 @@ export default function ProfilePage() {
 
       <ArticleSlider />
 
-      <Footer />
+      <Footer className="!pt-20" />
     </main>
   );
 }
