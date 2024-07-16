9346896e574def9790e442653d7908c8b3774a84
Ahmad Zaki Akmal <87590846+ahmadzakiakmal@users.noreply.github.com>
2024-07-16 08:33:35 +0700

    feat: done basic layout for article page

--- src/pages/article/[id].tsx
+++ src/pages/article/[id].tsx
@@ -1,11 +1,54 @@
+import Button from "@/components/Button";
 import { useRouter } from "next/router";
+import { MdInsertLink } from "react-icons/md";
+
+function ExternalLink() {
+  return (
+    <button className="bg-white rounded-[8px] shadow-[0_3px_4px_rgba(0,0,0,.4)] flex gap-3 items-center px-3 py-2 font-poppins hover:bg-[#F0F0F0] transition-colors active:bg-[#DEDEDE]">
+      <MdInsertLink className="text-[30px]" />
+      <span>Sosial Media</span>
+    </button>
+  );
+}
 
 export default function ArticlePage() {
-  const router = useRouter()
-  
-  return(
-    <main className="pt-[120px]">
-      {router.query.id}
+  const router = useRouter();
+
+  return (
+    <main className="pt-[200px] pb-[120px]">
+      <section className="flex flex-col justify-center items-center gap-8">
+        <div className="flex justify-center items-center font-poppins px-10 py-2 bg-green-1 rounded-full text-white">
+          Nama Desa
+        </div>
+
+        <div className="w-[620px] aspect-[620/415] bg-gradient-to-br from-green-2 from-20% to-yellow-1 rounded-[30px]" />
+
+        <div className="flex flex-col justify-center items-center">
+          <h1 className="text-green-1 font-bold text-[29px] md:text-[39px]">Artikel Nomor {router.query.id}</h1>
+          <h2 className="text-[16px] md:text-[21px]">Ditulis oleh: Penulis {router.query.id}</h2>
+        </div>
+      </section>
+
+      <section className="flex flex-col justify-center items-center mt-12">
+        <p className="text-justify max-w-[844px] text-[22px]">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore
+          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore
+          et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+          ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor
+          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+          laboris nisi ut aliquip ex ea commodo consequat.
+        </p>
+
+        <div className="mt-[50px] flex gap-5">
+          <ExternalLink />
+          <ExternalLink />
+          <ExternalLink />
+          <ExternalLink />
+        </div>
+
+        <Button ariaLabel="Back Button" className="mt-[60px]">Kembali</Button>
+      </section>
     </main>
-  )
+  );
 }
