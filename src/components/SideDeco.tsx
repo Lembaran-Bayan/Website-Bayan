4608b95b7d9b989de60d1ed5ad215be5845ab098
Ahmad Zaki Akmal <87590846+ahmadzakiakmal@users.noreply.github.com>
2024-08-12 21:43:42 +0800

    fix: side deco broken on very high pages

--- src/components/SideDeco.tsx
+++ src/components/SideDeco.tsx
@@ -6,8 +6,8 @@ export default function SideDeco({ position }: Props) {
   return (
     <div
       className={
-        "w-[31.1px] sm:w-[46.65px] md:w-[62.2px] bg-[url('/SideDeco.png')] absolute h-full bg-cover top-0 " +
-        (position ? "right-[-13px] scale-[-100%]" : "left-[-13px]")
+        "w-[31.1px] sm:w-[46.65px] md:w-[62.2px] bg-[url('/SideDeco.png')] absolute h-full bg-repeat-y top-0 " +
+        (position ? "right-[-13px] rotate-180" : "left-[-13px] bg-left")
       }
     ></div>
   );
